97c7c12e5397774e7e16e329c18abda5c7fe3402
Parsa <Parsa@DESKTOP-32S8QLS>
2025-03-30 20:33:50 +0330

    Made daily tasks unavailable until their due time

--- PromptCompiler/Program.cs
+++ PromptCompiler/Program.cs
@@ -14,6 +14,12 @@
 
             using (StreamWriter writer = new(outputFile))
             {
+                writer.WriteLine("Here's the entire code base of Tickly.");
+                writer.WriteLine("On top of each file, its name is written.");
+                writer.WriteLine("You might see the name written twice. That's a mistake.");
+                writer.WriteLine("It's a leftover of when file names were written in the file itself.");
+                writer.WriteLine("Exclude the file names when rewriting files. Those are for reference only and should not be part of the actual file content!");
+
                 foreach (string file in Directory.GetFiles(sourceDirectory, "*", SearchOption.AllDirectories))
                 {
                     if (Path.GetFileName(file).Equals("MergedOutput.txt", StringComparison.OrdinalIgnoreCase))
@@ -21,7 +27,7 @@
                         continue;
                     }
 
-                    writer.WriteLine("--- File: " + file[(sourceDirectory.Length + 1)..] + "---");
+                    writer.WriteLine("// File: " + file[(sourceDirectory.Length + 1)..]);
                     writer.WriteLine(File.ReadAllText(file));
                     writer.WriteLine();
                     writer.WriteLine(new string('-', 80));
---
+++ Tickly/Source/Converters/FutureRepeatingTaskIsEnabledConverter.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+using Tickly.Models;
+
+namespace Tickly.Converters;
+
+public class FutureRepeatingTaskIsEnabledConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values == null || values.Length < 2)
+            return true; // Default to enabled if bindings fail
+
+        var timeTypeObj = values[0];
+        var dueDateObj = values[1];
+
+        if (timeTypeObj is not TaskTimeType timeType)
+            return true; // Default to enabled if type is wrong
+
+        // Task is enabled if it's NOT a repeating task
+        if (timeType != TaskTimeType.Repeating)
+            return true;
+
+        // Task is enabled if it IS repeating but has NO due date (shouldn't happen?)
+        if (dueDateObj is not DateTime dueDate)
+            return true;
+
+        // Task is enabled if it IS repeating AND its due date is today or earlier
+        if (dueDate.Date <= DateTime.Today)
+            return true;
+
+        // Otherwise (Repeating Task with future DueDate), it is DISABLED
+        return false;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
